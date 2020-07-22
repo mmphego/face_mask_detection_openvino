2bfda4e8f86ea4e93fa20aff5c786d2a4a199d31
Mpho Mphego <mpho112@gmail.com>
2020-07-23 00:25:15 +0200

    Complete rework of the code

--- inference.py
+++ inference.py
@@ -1,104 +1,276 @@
-#!/usr/bin/env python3
-
 import os
+import math
 import sys
+import time
+import subprocess
 
+from abc import ABC, abstractmethod
+from pathlib import Path
+
+import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
+from loguru import logger
 from openvino.inference_engine import IENetwork, IECore
 
 
-class Network:
-    """
-    Load and configure inference plugins for the specified target devices
-    and performs synchronous and asynchronous modes for the specified infer requests.
-    """
+__all__ = [
+    "Face_Detection",
+    "Mask_Detection",
+]
 
-    def __init__(self):
-        # Initialize the plugin
-        # https://docs.openvinotoolkit.org/latest/ie_python_api/classie__api_1_1IECore.html
-        self.ie_core = IECore()
-        self.network = None
-        self.exec_network = None
-        self._input_blob = None
-        self._output_blob = None
 
-    def load_model(
-        self, model_xml: str, device: str = "CPU", cpu_extension=None
-    ) -> None:
-        """Load the model given IR files.
-#
-        Params
-        ======
-        model: str
-            model xml (Intermediate Representation)
-        device: str
-            Defaults to CPU as device for use in the workspace.
-        cpu_extension: str (optional)
-        """
-        ### TODO: Load the model ###
-        # https://docs.openvinotoolkit.org/latest/ie_python_api/classie__api_1_1IENetwork.html
-        model_bin = os.path.splitext(model_xml)[0] + ".bin"
-        assert os.path.isfile(model_bin) and os.path.isfile(model_xml)
-        self._model_size = os.stat(model_bin).st_size / 1024. ** 2
+class InvalidModel(Exception):
+    pass
 
-        # Add a CPU extension, if applicable
-        if cpu_extension and "CPU" in device:
-            self.ie_core.add_extension(cpu_extension, device)
 
-        try:
-            self.network = self.ie_core.read_network(model=model_xml, weights=model_bin)
-        except AttributeError:
-            self.network = IENetwork(model=model_xml, weights=model_bin)
+class Base(ABC):
+    """Model Base Class"""
 
-        # Load the IENetwork into the plugin
-        self.exec_network = self.ie_core.load_network(
-            network=self.network, device_name=device
+    def __init__(
+        self,
+        model_name,
+        source_width=None,
+        source_height=None,
+        device="CPU",
+        threshold=0.60,
+        extensions=None,
+    ):
+        self.model_weights = f"{model_name}.bin"
+        self.model_structure = f"{model_name}.xml"
+        assert (
+            Path(self.model_weights).absolute().exists()
+            and Path(self.model_structure).absolute().exists()
         )
 
-        ### TODO: Check for supported layers ###
-        supported_layers = self.ie_core.query_network(
-            network=self.network, device_name=device
-        )
-        # Check for any unsupported layers, and let the user know if anything is missing.
-        unsupported_layers = [
-            l for l in self.network.layers.keys() if l not in supported_layers
-        ]
-        if len(unsupported_layers) != 0:
-            msg = (
-                "Unsupported layers found: {}, Check whether extensions are available "
-                "to add to IECore.".format(unsupported_layers)
-            )
-            raise RuntimeError(msg)
+        self.device = device
+        self.threshold = threshold
+        self._model_size = os.stat(self.model_weights).st_size / 1024.0 ** 2
 
-        ### TODO: Add any necessary extensions ###
-        # Add a CPU extension, if applicable
-        if cpu_extension and "CPU" in device:
-            self.ie_core.add_extension(cpu_extension, device)
+        self._ie_core = IECore()
+        self.model = self._get_model()
 
         # Get the input layer
-        self._input_blob = next(iter(self.network.inputs))
-        self._output_blob = next(iter(self.network.outputs))
+        self.input_name = next(iter(self.model.inputs))
+        self.input_shape = self.model.inputs[self.input_name].shape
+        self.output_name = next(iter(self.model.outputs))
+        self.output_shape = self.model.outputs[self.output_name].shape
+        self._init_image_w = source_width
+        self._init_image_h = source_height
+        self.exec_network = None
+        self.perf_stats = {}
+        self.load_model()
 
-    def get_input_shape(self) -> list:
-        """Gets the input shape of the network."""
-        return self.network.inputs[self._input_blob].shape
+    def _get_model(self):
+        """Helper function for reading the network."""
+        try:
+            try:
+                model = self._ie_core.read_network(
+                    model=self.model_structure, weights=self.model_weights
+                )
+            except AttributeError:
+                logger.warn("Using an old version of OpenVINO, consider updating it!")
+                model = IENetwork(
+                    model=self.model_structure, weights=self.model_weights
+                )
+        except Exception:
+            raise ValueError(
+                "Could not Initialise the network. "
+                "Have you entered the correct model path?"
+            )
+        else:
+            return model
 
-    def exec_net(
-        self, image: object, request_id: int = 0,
-    ):
-        """Makes an asynchronous inference request, given an input image."""
+    def load_model(self):
+        """Load the model into the plugin"""
+        if self.exec_network is None:
+            start_time = time.time()
+            self.exec_network = self._ie_core.load_network(
+                network=self.model, device_name=self.device
+            )
+            self._model_load_time = (time.time() - start_time) * 1000
+            logger.info(
+                f"Model: {self.model_structure} took {self._model_load_time:.3f} ms to load."
+            )
+
+    def predict(self, image, request_id=0, show_bbox=False, **kwargs):
         if not isinstance(image, np.ndarray):
-            raise IOError("Image not parsed.")
+            raise IOError("Image not parsed correctly.")
+
+        p_image = self.preprocess_input(image, **kwargs)
+        predict_start_time = time.time()
         self.exec_network.start_async(
-            request_id=request_id, inputs={self._input_blob: image}
+            request_id=request_id, inputs={self.input_name: p_image}
+        )
+        status = self.exec_network.requests[request_id].wait(-1)
+        if status == 0:
+            pred_result = []
+            for output_name, data_ptr in self.model.outputs.items():
+                pred_result.append(
+                    self.exec_network.requests[request_id].outputs[output_name]
+                )
+            self.perf_stats[output_name] = self.exec_network.requests[
+                request_id
+            ].get_perf_counts()
+            predict_end_time = float(time.time() - predict_start_time) * 1000
+            bbox, _ = self.preprocess_output(pred_result, image, show_bbox=show_bbox)
+            return (predict_end_time, bbox)
+
+    @abstractmethod
+    def preprocess_output(self, inference_results, image, show_bbox=False, **kwargs):
+        """Draw bounding boxes onto the frame."""
+        raise NotImplementedError("Please Implement this method")
+
+    @staticmethod
+    @abstractmethod
+    def draw_output(image):
+        raise NotImplementedError("Please Implement this method")
+
+    @staticmethod
+    def plot_frame(image):
+        """Helper function for finding image coordinates/px"""
+        img = image[:, :, 0]
+        plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+        plt.show()
+
+    def add_text(self, text, image, position, font_size=0.75, color=(255, 255, 255)):
+        cv2.putText(
+            image, text, position, cv2.FONT_HERSHEY_COMPLEX, font_size, color, 1,
         )
 
-    def wait(self, request_id: int = 0) -> int:
-        """Checks the status of the inference request."""
-        status = self.exec_network.requests[request_id].wait(-1)
-        return status
+    def preprocess_input(self, image, height=None, width=None):
+        """Helper function for processing frame"""
+        if (height and width) is None:
+            height, width = self.input_shape[2:]
+        p_frame = cv2.resize(image, (width, height))
+        # Change data layout from HWC to CHW
+        p_frame = p_frame.transpose((2, 0, 1))
+        p_frame = p_frame.reshape(1, *p_frame.shape)
+        return p_frame
 
-    def get_output(self, request_id: int = 0):
-        """Returns a list of the results for the output layer of the network."""
-        return self.exec_network.requests[request_id].outputs[self._output_blob]
+
+class Face_Detection(Base):
+    """Class for the Face Detection Model."""
+
+    def __init__(
+        self,
+        model_name,
+        source_width=None,
+        source_height=None,
+        device="CPU",
+        threshold=0.60,
+        extensions=None,
+    ):
+        super().__init__(
+            model_name, source_width, source_height, device, threshold, extensions,
+        )
+
+    def preprocess_output(self, inference_results, image, show_bbox=False):
+        """Draw bounding boxes onto the Face Detection frame."""
+        if not (self._init_image_w and self._init_image_h):
+            raise RuntimeError("Initial image width and height cannot be None.")
+        if len(inference_results) == 1:
+            inference_results = inference_results[0]
+
+        coords = []
+        for box in inference_results[0][0]:  # Output shape is 1x1xNx7
+            conf = box[2]
+            if conf >= self.threshold:
+                xmin = int(box[3] * self._init_image_w)
+                ymin = int(box[4] * self._init_image_h)
+                xmax = int(box[5] * self._init_image_w)
+                ymax = int(box[6] * self._init_image_h)
+                coords.append((xmin, ymin, xmax, ymax))
+                if show_bbox:
+                    self.draw_output(image, xmin, ymin, xmax, ymax)
+        return coords, image
+
+    @staticmethod
+    def draw_output(
+        image,
+        xmin,
+        ymin,
+        xmax,
+        ymax,
+        label="Person's Face",
+        bbox_color=(0, 255, 0),
+        padding_size=(0.05, 0.25),
+        text_color=(255, 255, 255),
+        text_scale=2,
+        text_thickness=2,
+    ):
+
+        cv2.rectangle(
+            image, (xmin, ymin), (xmax, ymax,), color=bbox_color, thickness=2,
+        )
+
+        ((label_width, label_height), _) = cv2.getTextSize(
+            label,
+            cv2.FONT_HERSHEY_PLAIN,
+            fontScale=text_scale,
+            thickness=text_thickness,
+        )
+
+        cv2.rectangle(
+            image,
+            (xmin, ymin),
+            (
+                int(xmin + label_width + label_width * padding_size[0]),
+                int(ymin + label_height + label_height * padding_size[1]),
+            ),
+            color=bbox_color,
+            thickness=cv2.FILLED,
+        )
+        cv2.putText(
+            image,
+            label,
+            org=(xmin, int(ymin + label_height + label_height * padding_size[1]),),
+            fontFace=cv2.FONT_HERSHEY_PLAIN,
+            fontScale=text_scale,
+            color=text_color,
+            thickness=text_thickness,
+        )
+
+
+class Mask_Detection(Base):
+    """Class for the Mask Detection Model."""
+
+    def __init__(
+        self,
+        model_name,
+        source_width=None,
+        source_height=None,
+        device="CPU",
+        threshold=0.60,
+        extensions=None,
+    ):
+        super().__init__(
+            model_name, source_width, source_height, device, threshold, extensions,
+        )
+
+    def preprocess_output(self, inference_results, image, show_bbox=False, **kwargs):
+        flattened_predictions = np.vstack(inference_results).ravel()
+        if show_bbox:
+            self.draw_output(image, flattened_predictions)
+        return flattened_predictions, image
+
+    def draw_output(
+        self, image, inference_results, color={"Red": (0, 255, 0), "Green": (0, 0, 255)}
+    ):
+        label = (
+            ("Mask", color["Red"])
+            if float(inference_results) > self.threshold
+            else ("No Mask", color["Green"])
+        )
+        position = (int(image.shape[1] / 2 - 250 / 2), int(image.shape[0] / 2))
+
+        cv2.putText(
+            image,
+            label[0],
+            position,
+            cv2.FONT_HERSHEY_SIMPLEX,
+            fontScale=2,
+            color=label[1],
+            thickness=5,
+        )
--- main.py
+++ main.py
@@ -1,19 +1,144 @@
 #!/usr/bin/env python3
 
-import time
+import mimetypes
 import os
+import time
 
 from argparse import ArgumentParser
 
 import cv2
 import numpy as np
+
+from loguru import logger
 from responsive_voice.voices import UKEnglishMale
+from tqdm import tqdm
+
+from inference import Face_Detection, Mask_Detection
 
 
-from inference import Network
+class FormatNotSupported(Exception):
+    pass
 
-engine = UKEnglishMale()
-mp3_file = engine.get_mp3("Please wear your MASK!!")
+
+class InputFeeder:
+    def __init__(self, input_file=None):
+        """
+        This class can be used to feed input from an image, webcam, or video to your model.
+
+        Parameters
+        ----------
+        input_file: str
+            The file that contains the input image or video file.
+            Leave empty for cam input_type.
+
+        Example
+        -------
+        ```
+            feed=InputFeeder(input_file='video.mp4')
+            for batch in feed.next_frame():
+                do_something(batch)
+            feed.close()
+        ```
+        """
+        self.input_file = input_file
+        assert isinstance(self.input_file, str)
+        self.check_file_exists(self.input_file)
+        try:
+            self._input_type, _ = mimetypes.guess_type(self.input_file)
+            assert isinstance(self._input_type, str)
+        except AssertionError:
+            self._input_type = ""
+        self._progress_bar = None
+        self.load_data()
+
+    def load_data(self):
+        if "video" in self._input_type:
+            self.cap = cv2.VideoCapture(self.input_file)
+        elif "image" in self._input_type:
+            self.cap = cv2.imread(self.input_file)
+        elif "cam" in self.input_file.lower():
+            self._input_type = self.input_file
+            self.cap = cv2.VideoCapture(0)
+        else:
+            msg = f"Source: {self.input_file} not supported!"
+            logger.warn(msg)
+            raise FormatNotSupported(msg)
+        logger.info(f"Loaded input source type: {self._input_type}")
+
+    @staticmethod
+    def check_file_exists(file):
+        if "cam" in file:
+            return
+
+        if not os.path.exists(os.path.abspath(file)):
+            raise FileNotFoundError(f"{file} does not exist.")
+
+    @property
+    def source_width(self):
+        return int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+
+    @property
+    def source_height(self):
+        return int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    @property
+    def video_len(self):
+        return int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    @property
+    def fps(self):
+        return int(self.cap.get(cv2.CAP_PROP_FPS))
+
+    @property
+    def progress_bar(self):
+        if not self._progress_bar:
+            self._progress_bar = tqdm(total=int(self.video_len - self.fps + 1))
+        return self._progress_bar
+
+    def resize(self, frame, height=None, width=None):
+        if (height and width) is None:
+            width, height = (self.source_width // 2, self.source_height // 2)
+        return cv2.resize(frame, (width, height))
+
+    def show(self, frame, frame_name="video"):
+        cv2.imshow(frame_name, frame)
+
+    def write_video(self, output_path=".", filename="output_video.mp4"):
+        out_video = cv2.VideoWriter(
+            os.path.join(output_path, filename),
+            cv2.VideoWriter_fourcc(*"avc1"),
+            fps,
+            (self.source_width, self.source_height),
+            True,
+        )
+        return out_video
+
+    # TODO: Convert to contextmanager class
+    def next_frame(self, quit_key="q"):
+        """Returns the next image from either a video file or webcam."""
+        while self.cap.isOpened():
+            self.progress_bar.update(1)
+            flag = False
+            for _ in range(1):
+                flag, frame = self.cap.read()
+
+            if not flag:
+                break
+            yield frame
+
+            key = cv2.waitKey(1) & 0xFF
+            # if `quit_key` was pressed, break from the loop
+            if key == ord(quit_key):
+                break
+
+    def close(self):
+        """Closes the VideoCapture."""
+        if "image" in self._input_type:
+            self.cap.release()
+        if self.progress_bar:
+            self.progress_bar.close()
+        cv2.destroyAllWindows()
+        logger.info("============ CleanUp! ============")
 
 
 def arg_parser():
@@ -47,7 +172,6 @@ def arg_parser():
     parser.add_argument(
         "-d",
         "--device",
-        type=str,
         default="CPU",
         help="Specify the target device to infer on: "
         "CPU, GPU, FPGA or MYRIAD is acceptable. Sample "
@@ -55,17 +179,33 @@ def arg_parser():
         "specified (CPU by default)",
     )
     parser.add_argument(
-        "-pt",
-        "--prob_threshold",
+        "--face_prob_threshold",
         type=float,
         default=0.8,
-        help="Probability threshold for detections filtering" "(0.8 by default)",
+        help="Probability threshold for face detections filtering (Default: 0.8)",
     )
     parser.add_argument(
-        "--out", action="store_true", help="Write video to file.",
+        "--mask_prob_threshold",
+        type=float,
+        default=0.3,
+        help="Probability threshold for face mask detections filtering"
+        "(Default: 0.3)",
+    )
+    parser.add_argument(
+        "--enable-speech", action="store_true", help="Enable speech notification.",
+    )
+    parser.add_argument(
+        "--tts",
+        default="Please wear your MASK!!",
+        help="Text-to-Speech, used for notification.",
     )
     parser.add_argument(
         "--ffmpeg", action="store_true", help="Flush video to FFMPEG.",
+    )
+    parser.add_argument(
+        "--show-bbox",
+        action="store_true",
+        help="Show bounding box and stats on screen [debugging].",
     )
     parser.add_argument(
         "--debug", action="store_true", help="Show output on screen [debugging].",
@@ -74,48 +214,7 @@ def arg_parser():
     return parser.parse_args()
 
 
-def draw_boxes(frame, f_result, m_result, count, prob_threshold, width, height):
-    """Draw bounding boxes onto the frame."""
-    loc = 20
-    for box in f_result[0][0]:  # Output shape is 1x1x100x7
-        conf = box[2]
-        if conf >= prob_threshold:
-            xmin = int(box[3] * width)
-            ymin = int(box[4] * height)
-            xmax = int(box[5] * width)
-            ymax = int(box[6] * height)
-            _y = ymax + loc if ymax + loc > loc else ymax - loc
-
-            detected_threshold = round(float(m_result.flatten()), 3)
-            if detected_threshold > 0.3:
-                label = ("Mask", (0, 255, 0))  # Color format: BGR
-            else:
-                label = ("No Mask", (0, 0, 255))
-                if int(count) % 200 == 1:
-                    engine.play_mp3(mp3_file)
-            cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), label[1], 1)
-            cv2.putText(
-                frame,
-                f"{label[0]}: {detected_threshold :.2f}%",
-                (xmin - 2 * loc, _y),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                fontScale=0.5,
-                color=label[1],
-                thickness=2,
-            )
-    return frame
-
-
-def process_frame(frame, height, width):
-    """Helper function for processing frame"""
-    p_frame = cv2.resize(frame, (width, height))
-    # Change data layout from HWC to CHW
-    p_frame = p_frame.transpose((2, 0, 1))
-    p_frame = p_frame.reshape(1, *p_frame.shape)
-    return p_frame
-
-
-def infer_on_stream(args):
+def main(args):
     """
     Initialize the inference network, stream video to network,
     and output stats and video.
@@ -124,97 +223,82 @@ def infer_on_stream(args):
     :param client: MQTT client
     :return: None
     """
+    # Initialise the video stream
+    video_feed = InputFeeder(input_file=args.input)
+    # Initialise the speech output
+    if args.enable_speech:
+        # TODO: Add args for selecting language, accent and male/female voice
+        engine = UKEnglishMale()
+        speak = engine.get_mp3(args.tts)
+
     # Initialise the class
-    face_infer_network = Network()
-    mask_infer_network = Network()
-    # Set Probability threshold for detections
-    prob_threshold = args.prob_threshold
+    face_detection = Face_Detection(
+        model_name=args.face_model,
+        source_width=video_feed.source_width,
+        source_height=video_feed.source_height,
+        device=args.device,
+        threshold=args.face_prob_threshold,
+    )
+    mask_detection = Mask_Detection(
+        model_name=args.mask_model,
+        device=args.device,
+        threshold=args.mask_prob_threshold,
+    )
 
     try:
-        mask_infer_network.load_model(
-            model_xml=args.mask_model,
-            device=args.device,
-        )
-        face_infer_network.load_model(
-            model_xml=args.face_model,
-            device=args.device,
-        )
-    except Exception:
-        raise
+        count = 0
+        # TODO: Convert to contextmanager
+        for frame in video_feed.next_frame():
+            count += 1
 
-    if args.input.lower() == "cam":
-        video_file = 0
-    else:
-        video_file = args.input
-        assert os.path.isfile(video_file)
-
-    stream = cv2.VideoCapture(video_file)
-    stream.open(video_file)
-
-    # Grab the shape of the input
-    orig_width = int(stream.get(3))
-    orig_height = int(stream.get(4))
-    average_infer_time = []
-
-    _, _, input_height, input_width = face_infer_network.get_input_shape()
-    _, _, mask_input_height, mask_input_width = mask_infer_network.get_input_shape()
-
-    if not stream.isOpened():
-        msg = "Cannot open video source!!!"
-        raise RuntimeError(msg)
-    count = 0
-
-    while stream.isOpened():
-        # Grab the next stream.
-        (grabbed, frame) = stream.read()
-        # If the frame was not grabbed, then we might have reached end of steam,
-        # then break
-        if not grabbed:
-            break
-
-        count += 1
-        p_frame = process_frame(frame, input_height, input_width)
-        m_frame = process_frame(frame, mask_input_height, mask_input_width)
-
-        start_infer = time.time()
-        face_infer_network.exec_net(p_frame)
-        mask_infer_network.exec_net(m_frame)
-        if face_infer_network.wait() == 0 and mask_infer_network.wait() == 0:
-            f_result = face_infer_network.get_output()
-            m_result = mask_infer_network.get_output()
-            end_infer = time.time() - start_infer
-            average_infer_time.append(end_infer)
-            message = f"Inference time: {end_infer*1000:.2f}ms"
-            cv2.putText(
-                frame, message, (20, 20), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0), 1
+            face_detect_infer_time, face_bboxes = face_detection.predict(
+                frame, show_bbox=args.show_bbox
             )
+            if face_bboxes:
+                for face_bbox in face_bboxes:
+                    # Useful resource: https://www.pyimagesearch.com/2018/09/24/opencv-face-recognition/
 
-            # Draw the boxes onto the input
-            out_frame = draw_boxes(
-                frame,
-                f_result,
-                m_result,
-                count,
-                prob_threshold,
-                orig_width,
-                orig_height,
-            )
+                    # Face bounding box coordinates cropped from the face detection inference
+                    # are face_bboxes i.e `xmin, ymin, xmax, ymax`
+                    # Therefore the face can be cropped by:
+                    # frame[face_bbox[1]:face_bbox[3], face_bbox[0]:face_bbox[2]]
 
-        if args.debug:
-            cv2.imshow("Frame", frame)
+                    # extract the face ROI
+                    (x, y, w, h) = face_bbox
+                    face = frame[y:h, x:w]
+                    (face_height, face_width) = face.shape[:2]
+                    # Crop and show face
+                    # video_feed.show(frame[y:h, x:w], "face")
 
-        key = cv2.waitKey(1) & 0xFF
+                    # ensure the face width and height are sufficiently large
+                    if face_height < 20 or face_width < 20:
+                        continue
 
-        # # if the `q` key was pressed, break from the loop
-        if key == ord("q"):
-            break
+                    mask_detect_infer_time, mask_bboxes = mask_detection.predict(
+                        face, show_bbox=args.show_bbox
+                    )
 
-    # Release the out writer, capture, and destroy any OpenCV windows
-    stream.release()
-    cv2.destroyAllWindows()
+                    if int(count) % 200 == 1 and args.enable_speech:
+                        engine.play_mp3(mp3_file)
+
+            if args.debug:
+                text = f"Face Detection Inference time: {face_detect_infer_time:.3f} ms"
+                face_detection.add_text(
+                    text, frame, (15, video_feed.source_height - 80)
+                )
+                text = f"Face Mask Detection Inference time: {mask_detect_infer_time:.3f} ms"
+                mask_detection.add_text(
+                    text, frame, (15, video_feed.source_height - 60)
+                )
+
+                video_feed.show(video_feed.resize(frame))
+
+    finally:
+        video_feed.close()
+
 
 if __name__ == "__main__":
     # Grab command line args
     args = arg_parser()
     # Perform inference on the input stream
-    infer_on_stream(args)
+    main(args)
